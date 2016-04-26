debbc3deabc61721d37e5016019cac0a1c580f4d
rosarywang <wangr@mccombs.utexas.edu>
2016-04-26 14:32:39 -0500

    add ball back in after setting

--- app/src/main/java/team6/photoball/Gallery.java
+++ app/src/main/java/team6/photoball/Gallery.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import java.io.File;
 import java.io.IOException;
@@ -166,7 +168,7 @@ public class Gallery extends Fragment {
             try {
                 ProcessTask.mBitmap = BitmapFactory.decodeFile(this.mImageFile.getAbsolutePath());
                 ProcessTask.initRotateImageIfRequired();
-//               ((SimulationClass) bouncingBallView).setBitmap(ProcessTask.mImageView.getDrawable());
+               ((SimulationClass) bouncingBallView).setBitmap((ImageView) getView().findViewById(R.id.imageViewGallery));
             } catch (IOException e) {
                 e.printStackTrace();
             }
